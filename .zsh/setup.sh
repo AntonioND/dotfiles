076a7385d35dc1c78cf0d70e73efdbe730057f96
Antonio Niño Díaz <antonio_nd@outlook.com>
2017-09-12 21:02:20 +0100

    zsh: Download plugins used by Oh My Zsh
    They aren't downloaded automatically.
    
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- .zsh/setup.sh
+++ .zsh/setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+#Oh My Zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions oh-my-zsh/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/mollifier/cd-gitroot oh-my-zsh/plugins/cd-gitroot
+git clone https://github.com/psprint/zsh-cmd-architect oh-my-zsh/plugins/zsh-cmd-architect
+git clone https://github.com/RobSis/zsh-completion-generator oh-my-zsh/plugins/zsh-completion-generator
