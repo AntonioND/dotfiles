cbfcac575a1ed76e262c6fcd4fa1b99ef5db4fac
Antonio Niño Díaz <antonio_nd@outlook.com>
2017-09-19 00:59:49 +0100

    vim: Add script to download git repositories
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

---
+++ vim-plugins/download-plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Download plugins
+git clone https://github.com/terryma/vim-multiple-cursors
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin
