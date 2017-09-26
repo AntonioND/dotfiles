dd70f3a75131f02df620af063abb206b0b376a49
Antonio Niño Díaz <antonio_nd@outlook.com>
2017-09-26 01:20:23 +0100

    tmux: Add CPU/battery information to status bar
    Add dependencies to setup.sh script.
    
    Make messages be displayed with a bold font.
    
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- .tmux/setup.sh
+++ .tmux/setup.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+
+# Copy/paste from/to clipboard
+sudo apt-get install xsel
+
+###########
+# Plugins #
+###########
 git clone https://github.com/NHDaly/tmux-better-mouse-mode
+
+git clone https://github.com/tmux-plugins/tmux-battery
+
+sudo apt-get install sysstat
+git clone https://github.com/tmux-plugins/tmux-cpu
