b745243c098edc73640dd20be2e8e38f334af81c
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-03-19 23:13:58 +0000

    nvim: vim: Add ALE linter plugin
    The plugin has been configurated so that it doesn't run all the time
    unnecessarily.
    
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- vim-plugins/download-plugins.sh
+++ vim-plugins/download-plugins.sh
@@ -7,3 +7,7 @@ git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin
+
+# Install ALE
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone https://github.com/w0rp/ale.git ~/.vim/pack/git-plugins/start/ale
