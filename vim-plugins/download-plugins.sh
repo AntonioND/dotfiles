bfb9ccb1883acfd19bf8271189805380ec7c1bbe
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-04-03 01:43:53 +0100

    vim: Remove vim-multiple-cursors
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- vim-plugins/download-plugins.sh
+++ vim-plugins/download-plugins.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Download plugins
-git clone https://github.com/terryma/vim-multiple-cursors
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/airblade/vim-gitgutter
