46b5a2692bc95db7c95fec41bc80ff48560a1397
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-04-12 23:53:07 +0100

    vim: Remove NERDTree
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- vim-plugins/download-plugins.sh
+++ vim-plugins/download-plugins.sh
@@ -4,5 +4,3 @@
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/airblade/vim-gitgutter
-git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin
