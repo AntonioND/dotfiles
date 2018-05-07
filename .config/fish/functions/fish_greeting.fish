0b7dab2d6dd0a6ef3ee38c85cdb93133c118bbae
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-05-08 00:36:45 +0100

    fish: Remove config files
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- .config/fish/functions/fish_greeting.fish
+++
@@ -1,6 +0,0 @@
-function fish_greeting
-    set_color --bold white
-    echo (uname -srvmo)
-    echo
-    set_color normal
-end
--- .config/fish/functions/git_get_branch.fish
+++
@@ -1,9 +0,0 @@
-# Get a string describing the current HEAD if we are in a git repository
-function git_get_branch
-    if git rev-parse --git-dir > /dev/null ^&1
-        # This is a valid git repository (but maybe not the top level).
-        git describe --all ^ /dev/null
-    else
-        echo ""
-    end
-end
